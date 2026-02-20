f088634f563886880b004cd8df3e0a5f94ddbcd9
Junsu Jeon <pds.jeon@gmail.com>
2026-02-20 13:51:22 +0900

    separated swagger html file

--- Swiftness/Sources/Swiftness/routes.swift
+++ Swiftness/Sources/Swiftness/routes.swift
@@ -9,29 +9,10 @@ func routes(_ app: Application) throws {
     try handler.registerHandlers(on: transport)
     
     app.get("swagger") { req -> Response in
-        let html = """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="utf-8" />
-          <title>Swagger UI</title>
-          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
-          <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
-        </head>
-        <body>
-          <div id="swagger-ui"></div>
-          <script>
-            window.onload = () => {
-              window.ui = SwaggerUIBundle({
-                url: '/openapi.yaml', // Path to the OpenAPI spec file
-                dom_id: '#swagger-ui',
-              });
-            };
-          </script>
-        </body>
-        </html>
-        """
-        return Response(status: .ok, headers: ["Content-Type": "text/html"], body: .init(string: html))
+        guard let path = Bundle.module.path(forResource: "swagger", ofType: "html") else {
+            throw Abort(.notFound, reason: "Could not find swagger.html in the bundle.")
+        }
+        return try await req.fileio.asyncStreamFile(at: path)
     }
     app.get("openapi.yaml") { req -> Response in
             // Use Bundle.module to dynamically find the path to the bundled file.
